75e21021aab484a5a3a4e9ace1a87082c20c0fda
Rune M. Andersen <rune.andersen@gmail.com>
2022-12-15 09:20:47 +0100

    Added extensions to start activities.

---
+++ library/src/main/java/no/wtw/android/architectureutils/activityarguments/IntentBuilderExt.kt
@@ -0,0 +1,45 @@
+package no.wtw.android.architectureutils.activityarguments
+
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import androidx.core.app.ActivityCompat
+import androidx.fragment.app.Fragment
+import kotlin.reflect.KClass
+
+fun <T : Activity> KClass<T>.intent(activity: Activity): IntentBuilder<T> {
+    return IntentBuilder(activity, null, this)
+}
+
+fun <T : Activity> KClass<T>.intent(fragment: Fragment): IntentBuilder<T> {
+    return IntentBuilder(fragment.requireContext(), fragment, this)
+}
+
+class IntentBuilder<T : Activity>(
+    private val ctx: Context,
+    private val fragment: Fragment?,
+    clazz: KClass<T>
+) {
+
+    val intent = Intent(ctx, clazz.java)
+
+    fun putArguments(bundleData: BundleData): IntentBuilder<T> {
+        intent.putArguments(bundleData)
+        return this
+    }
+
+    fun get() = intent
+    fun start() = ctx.startActivity(intent)
+
+    @Deprecated(message = "Deprecated", replaceWith = ReplaceWith("registerForActivityResult"))
+    fun startForResult(requestCode: Int) {
+        if (fragment != null) {
+            fragment.startActivityForResult(intent, requestCode)
+        } else if (ctx is Activity) {
+            ActivityCompat.startActivityForResult(ctx, intent, requestCode, null)
+        } else {
+            ctx.startActivity(intent)
+        }
+    }
+
+}
